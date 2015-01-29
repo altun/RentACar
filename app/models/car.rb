44bd41b90657e80d8b9aea2fe9b07960da0e8c18
Emrah Altun <emrah.altun@uzem.omu.edu.tr>
2015-01-29 16:39:18 +0200

    minor

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,10 +1,10 @@
 class Car < ActiveRecord::Base
-	validates :make, :model, length: {in: 2..50}
+	validates :make, :model, length: {in: 2..50, too_short: "en az %{count} karakter girmelisiniz.", too_long: "en fazla %{count} karakter girebilirsiniz."}
 	# Bir diğer kullanım
 	# validates :make, :model, length: {minimun: 2, maximum: 50}
 
-	validates :vin, uniqueness: {case_sensitive: false}, presence: true
+	validates :vin, uniqueness: {case_sensitive: false, message: "Bu alan benzersiz olmalıdır."}, presence: {message: "Bu alanı boş bırakamazsınız."}
 	validates :year, length: {is: 4}
-	validates :year, numericality: {only_integer: true, less_than_or_equal_to: Time.now.year}
+	validates :year, inclusion: {in: 1990..Time.now.year}, allow_nil: true
 	validates :doors_count, numericality: {only_integer: true, odd: true}
 end
